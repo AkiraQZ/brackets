4c0dea73f5c1932c74369ec9998dcc7ed7166aa5
AkiraQZ <vboyrkin@gmail.com>
2023-10-29 22:19:42 +0800

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+      return false;
+    }
+
+    const openingBrackets = bracketsConfig.map(pair => pair[0]);
+    const closingBrackets = bracketsConfig.map(pair => pair[1]);
+
+    for (let i = 0; i < openingBrackets.length; i++) {
+      const openingBracket = openingBrackets[i];
+      const closingBracket = closingBrackets[i];
+      const index = str.indexOf(openingBracket);
+
+      if (index !== -1) {
+        const nextIndex = str.indexOf(closingBracket, index + 1);
+
+        if (nextIndex !== -1) {
+          const inside = str.slice(index + 1, nextIndex);
+          const outside = str.slice(0, index) + str.slice(nextIndex + 1);
+
+          return check(inside, bracketsConfig) && check(outside, bracketsConfig);
+        }
+      }
+    }
+
+    return str.length === 0;
+  }
